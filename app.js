1ebefc90e6b40893544fdaace1ebd7ee07c8df63
PingSharp <dotping@online.de>
2019-07-23 10:35:35 +0200

    first try with koa hello world

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Project entry file
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'Hello World from koa!';
+});
+ app.listen(3000,()=>{
+     console.log("server is listening to port 3000! ")
+ });
